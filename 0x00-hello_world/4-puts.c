74792a3a69363882846a8830ef831561d53c617f
Aosgos <segunad1997@gmail.com>
2022-09-08 21:41:04 -0700

    C Hello World

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #!/bin/bash
 #include <stdio.h>
 /**
- * main - main block
- * Return: 0
- */
+* main - main block
+* Return: 0
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");

2ce837054118dfb1c89a012f673d5bc52cfca2c9
Aosgos <segunad1997@gmail.com>
2022-09-12 18:05:05 -0700

    C-Hello World

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,14 @@
 #include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
  *
- * Always return 0 (success)
+ * Return: 1
  */
 
 int main(void)
 {
-	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }

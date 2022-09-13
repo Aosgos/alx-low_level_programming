d4c9dfb957cb05310633d5118f116fee71f751c1
Aosgos <segunad1997@gmail.com>
2022-09-12 19:28:37 -0700

    If Else Var

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * no special discription
+ * 
+ * Return: alway returns 0
+*/
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z' ; a++)
+		putchar(a);
+	for (a = 'A'; a <= 'Z'; a++)
+		putchar(a);
+	putchar('\n');
+	return (0);
+}

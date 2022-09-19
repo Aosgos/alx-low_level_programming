387a9cc0c9dc34555dabe4ac7d56d0adcf805c06
Aosgos <segunad1997@gmail.com>
2022-09-19 13:52:26 -0700

    Task 0 Finally Dusted!

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - main entry to point 'n' to its pointer
+ *
+ * @n: Prototype give to change the value of a pointers main location
+ * Return: return nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}

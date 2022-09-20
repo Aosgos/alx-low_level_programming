c6ab2faa4298cc35e1b1b2055af90186cd4c57b5
Aosgos <segunad1997@gmail.com>
2022-09-19 21:14:21 -0700

    Task 2 Finally Dusted!

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts - Entry point
+ * _puts: Main Entry point to function
+ * @str: Prototype given
+ */
+
+void _puts(char *str)
+{
+	int i = 0;
+
+	while (i >= 0)
+	{
+		if (*(str + i) == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+
+	_putchar(*(str + i));
+	i++;
+	}
+}

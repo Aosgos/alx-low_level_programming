cde5375bf25467b6bfc29e444cfed16de59b5ce0
Aosgos <segunad1997@gmail.com>
2022-09-21 13:33:27 -0700

    Task 0 Finally Dusted!

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - a function that concatinates two strings
+ * including the terminaing null byte, to the
+ * buffer pointed to by the destination
+ * @dest: protoype given for destinaition
+ * @src: prototype given for source file
+ * n: integer function figure
+ */
+
+char char *_strncat(char *dest, char *src, int n);
+{
+	strcat(dest, src);
+	return (dest);
+}

f032aba86b7bb095650a1c55c6c32bfee9a897ba
Aosgos <segunad1997@gmail.com>
2022-09-23 19:16:47 -0700

    Task 6 Fix

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *cap_string - main entry point to cap letters
+ * @s: pointers pointing address to caps some ass
+ * Return: 0, ./rest
+ */
+
+char *leet(char *)
+{
+	int i, j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+
+	return (s);
+}

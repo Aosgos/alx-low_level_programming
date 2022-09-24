817dcec92fae5f99a55368a3349f4068bd4069aa
Aosgos <segunad1997@gmail.com>
2022-09-23 19:45:26 -0700

    Tast 101 to Print Integers

---
+++ 0x06-pointers_arrays_strings/101-print-number.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+void print_number(int n)
+{
+	unsigned int m;
+
+	if (n < 0)
+	{
+		m = -n;
+		_putchar('-');
+	}
+	else
+	{
+		m = n;
+	}
+
+	if (m / 10)
+		print_number(m / 10);
+	_putchar((m % 10) + '0');
+}

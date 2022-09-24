d8bbdc14351bde0063c2afec4c532f1c2a09ba1f
Aosgos <segunad1997@gmail.com>
2022-09-23 17:31:41 -0700

    Task 4 Fix

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -1,4 +1,4 @@
-#nclude "main.h"
+#include "main.h"
 
 /**
  * reverse_array - Reverses the content of an array.
@@ -8,15 +8,13 @@
 
 void reverse_array(int *a, int n)
 {
-		int i, j;
-			int tmp;
+	int i, j, tmp;
 
-				for (i = 0; i < n / 2; i++)
-						{
-									j = (n - 1) - i;
-											tmp = a[i];
-													a[i] = a[j];
-															a[j] = tmp;
-
-																}
+	for (i = 0; i < n / 2; i++)
+	{
+		j = (n - 1) - i;
+		tmp = a[i];
+		a[i] = a[j];
+		a[j] = tmp;
+	}
 }

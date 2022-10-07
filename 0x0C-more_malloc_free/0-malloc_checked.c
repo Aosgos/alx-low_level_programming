ad9230805d01033d6e9a40cb249f43158b644bc7
Aosgos <segunad1997@gmail.com>
2022-10-06 21:49:23 -0700

    More Malloc Task 1

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: unsigned int
+ * Return: pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	char i;
+	
+	i = malloc(b);
+
+	if (s == NULL)
+		exit(98);
+
+	return (i);
+}

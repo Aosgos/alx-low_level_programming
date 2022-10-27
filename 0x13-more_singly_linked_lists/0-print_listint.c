9ecfb322ffd5205f248b5f9edb09a9367f56d890
Aosgos <59194295+Aosgos@users.noreply.github.com>
2022-10-27 05:56:59 +0100

    Update 0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,25 +2,21 @@
 
 /**
  * print_listint - print elements of listint_t
- *
  * @h: number of nodes
  * Return: number of nodes
  */
 
 size_t print_listint(const listint_t *h)
 {
-	size_t elements = 0;
-	
-	while (h)
-	{
-		printf(%d/n", h->next)
-		elements++;
-		h = h->next;
-	}
-	if (!h)
-	{
-		return (elements);
-	}
+        size_t elements = 0;
 
-	return (elements);
-}
+        while (h)
+        {
+                printf("%d/n", h->n);
+                h = h->next;
+                elements++;
+        }
+        if (!h)
+        {
+                return (elements);
+        }

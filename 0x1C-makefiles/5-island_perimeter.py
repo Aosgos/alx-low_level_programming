64b98fe1969a48b77ca1ff4eb1d1173533aaab90
Aosgos <segunad1997@gmail.com>
2023-03-03 03:11:38 -0800

    4th Makefile 😎😍

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,23 +1,34 @@
 #!/usr/bin/python3
+"""Defines an island perimeter measuring function."""
+
 
 def island_perimeter(grid):
-    """
-    Compute the perimeter of the island described in grid.
-    """
-    rows = len(grid)
-    cols = len(grid[0])
-    perimeter = 0
+    """Return the perimiter of an island.
+    The grid represents water by 0 and land by 1.
 
-    for i in range(rows):
-        for j in range(cols):
-            if grid[i][j] == 1:
-                if i == 0 or grid[i-1][j] == 0:
-                    perimeter += 1
-                if i == rows-1 or grid[i+1][j] == 0:
-                    perimeter += 1
-                if j == 0 or grid[i][j-1] == 0:
-                    perimeter += 1
-                if j == cols-1 or grid[i][j+1] == 0:
-                    perimeter += 1
+    Args:
+        grid (list): A list of list of integers representing an island.
+    Returns:
+        The perimeter of the island defined in grid.
+    """
+    height, width = 0, 0
 
-    return perimeter
+    num_rows = len(grid)
+    num_cols = len(grid[0])
+
+    for r in range(num_rows):
+        for c in range(num_cols):
+            if grid[r][c] == 1:
+                # Count the top and bottom sides of the island
+                if r == 0 or grid[r-1][c] == 0:
+                    height += 1
+                if r == num_rows-1 or grid[r+1][c] == 0:
+                    height += 1
+
+                # Count the left and right sides of the island
+                if c == 0 or grid[r][c-1] == 0:
+                    width += 1
+                if c == num_cols-1 or grid[r][c+1] == 0:
+                    width += 1
+
+    return (height + width)

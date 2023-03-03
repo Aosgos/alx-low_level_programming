6127a467d3cdb19c80c7405e8404f907b6f4dcf9
Aosgos <segunad1997@gmail.com>
2023-03-03 02:51:55 -0800

    4th Makefile 😎😍

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+def island_perimeter(grid):
+    """
+    Compute the perimeter of the island described in grid.
+    """
+    rows = len(grid)
+    cols = len(grid[0])
+    perimeter = 0
+
+    for i in range(rows):
+        for j in range(cols):
+            if grid[i][j] == 1:
+                if i == 0 or grid[i-1][j] == 0:
+                    perimeter += 1
+                if i == rows-1 or grid[i+1][j] == 0:
+                    perimeter += 1
+                if j == 0 or grid[i][j-1] == 0:
+                    perimeter += 1
+                if j == cols-1 or grid[i][j+1] == 0:
+                    perimeter += 1
+
+    return perimeter

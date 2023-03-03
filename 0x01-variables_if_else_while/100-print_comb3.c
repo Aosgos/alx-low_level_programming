50e902f8bd6bb9514772621d26acafd5f2f852c6
Aosgos <segunad1997@gmail.com>
2023-03-03 14:56:14 -0800

    Printing all possible numbers of a 2 digit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -8,19 +8,16 @@
 int main(void)
 {
     int i, j;
-
+d
 	for (i = 0; i <= 8; i++)
 	{
-		while (
 		for (j = i + 1; j <= 9; j++)
 		{
 			putchar(i + '0');
 			putchar(j + '0');
-			/**putchar(',');
-			 * putchar(' ');*/
+			putchar(',');
+			putchar(' ');
 			}
-		      )
-			putchar(' ') || putchar(',');
 	}
 
 	return (0);

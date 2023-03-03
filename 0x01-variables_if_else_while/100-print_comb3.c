60f4b794add92a23d7a42bb0ce935da47942ef07
Aosgos <59194295+Aosgos@users.noreply.github.com>
2023-03-03 23:36:49 +0100

    Update 100-print_comb3.c
    Newww

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,25 +1,21 @@
 #include <stdio.h>
 
-/**
- * main - main function
- *
- * Return: 0
- */
+int main() {
+    int i, j, k;
 
-int main(void)
-{
-	int i, j;
+    for (i = 0; i <= 7; i++) {
+        for (j = i + 1; j <= 8; j++) {
+            for (k = j + 1; k <= 9; k++) {
+                putchar(i + '0');
+                putchar(j + '0');
+                putchar(k + '0');
+                if (i < 7 || j < 8 || k < 9) {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
 
-	for (i = 0; i <= 8; i++)
-	{
-		for (j = i + 1; j <= 9; j++)
-		{
-			putchar(i + '0');
-			putchar(j + '0');
-			putchar(',');
-			putchar(' ');
-		}
-	}
-
-	return (0);
+    return 0;
 }
